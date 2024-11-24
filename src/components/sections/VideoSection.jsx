5fa9fb544e2ef2571993c4b92c32575cbc288c9b
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-11-24 22:20:02 +0100

    .

--- src/components/sections/VideoSection.jsx
+++ src/components/sections/VideoSection.jsx
@@ -10,6 +10,7 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), {
   ssr: false,
 });
+import Logo from "../Logo";
 export default function VideoSection() {
   const container = useRef(null);
 
@@ -27,13 +28,17 @@ export default function VideoSection() {
   const filter = useMotionTemplate`blur(${blur}px) brightness(${brightness}) saturate(${saturate})`;
 
   const scale = useTransform(scrollYProgress, [0, 0.5, 1], [1.8, 1.5, 1]);
+  const logoY = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
+  console.log(
+    "IN VIDEOSECTION.JSX KEEP BG VIDEO 100VH WITH TEXT ON OP TOP THAT TRANSLATES LIKE THE VIDEO ON SCROILL FROM PREVIOUS SECTION AS WELL AND MAKE THE CONTENTS THAT TELL INFO APPEAR IN A SCALE UP ANIMATION ON SCROLL"
+  );
   return (
     <section
       ref={container}
-      className="h-screen relative z-50  bg-black text-white mb-"
+      className="h-[200vh] relative z-50  bg-black text-white mb-"
     >
       <motion.div
-        className="relative size-full"
+        className="relative h-full flex flex-col"
         style={{
           y,
           maskImage,
@@ -51,13 +56,39 @@ export default function VideoSection() {
           volume={0}
           width="100%"
           height="100%"
-          className="react-player"
+          className="react-player h-screen relative"
           onReady={() => {
             setTimeout(() => {
               //setLoaded();
             }, 1500);
           }}
         />
+        <div className=" h-screen relative z-50 p-4 ">
+          <div className="flex flex-row items-end mb-14">
+            <motion.div
+              style={{ logoY }}
+              className="h-32 w-full absolute mx-auto"
+            >
+              <Logo />
+            </motion.div>
+
+            <h1 className="text-4xl ml-5">Amazon Rainforest</h1>
+          </div>
+          <div className="max-w-3xl mb-10">
+            <p className="text-3xl leading-10">
+              An immersive and interactive experience. Built for educators,
+              students and anyone passionate about nature and its wonders.
+            </p>
+          </div>
+          <div className="max-w-4xl">
+            <p className="text-3xl leading-10 ">
+              Amazon Rainforest was born from the purpose of bringing the beauty
+              and diversity of the world´s largest tropical florest, offering an
+              unique experience, where users can explore and have an interactive
+              learning experience.
+            </p>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
--- src/components/ui/GooeyButton.jsx
+++ src/components/ui/GooeyButton.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { ArrowBigRight } from "lucide-react";
 import { cn } from "@/lib/cn";
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 
 export const GooeyButton = ({ children, ...props }) => {
   const letters = children.split("");
@@ -18,102 +18,29 @@ export const GooeyButton = ({ children, ...props }) => {
               scale: 1,
               transition: {
                 delay: 1,
-                duration: 1,
-                ease: [0.35, 0.39, 0, 0.86],
               },
             },
-            hover: {
+            whilehover: {
+              scale: 1.1,
+            },
+            whiletap: {
               scale: 0.9,
             },
           }}
           initial="initial"
           animate="animate"
-          whileHover="hover"
-          className="relative group  bg-secondary text-secondaryForeground inline-flex font-bold px-6 py-0 rounded-xl text-xl leading-5 h-16 items-center"
+          whileHover="whilehover"
+          whileTap="whiletap"
+          transition={{
+            type: "tween",
+            duration: 0.3,
+            ease: [0.165, 0.84, 0.44, 1],
+          }}
+          className="relative group  bg-secondary text-secondaryForeground inline-flex font-bold px-7 py-5 rounded-full text-xl leading-none tracking-wider items-center"
         >
-          {letters.map((letter, i) => (
-            <span key={i} className="relative flex overflow-hidden">
-              <motion.span
-                className="relative"
-                variants={{
-                  initial: {
-                    y: "100%",
-                  },
-                  animate: {
-                    y: 0,
-                  },
-                }}
-                initial="initial"
-                animate="animate"
-                transition={{
-                  delay: 1 + i * 0.1,
-                  duration: 0.75,
-                  ease: [0.45, 0.19, 0.07, 0.8],
-                }}
-              >
-                {letter}
-              </motion.span>
-            </span>
-          ))}
-
-          <div className="absolute left-0 !z-[-10] grid place-items-center size-full">
-            <motion.div
-              variants={{
-                initial: {
-                  scale: 0,
-                  x: "0%",
-                },
-                animate: {
-                  scale: 0,
-                  x: "0%",
-                },
-                hover: {
-                  scale: [null, 1, 1],
-                  x: "200%",
-                  transition: {
-                    type: "tween",
-                    duration: 1.5,
-                    ease: [0.35, 0.39, 0, 0.86],
-                  },
-                },
-              }}
-              transition={{
-                type: "tween",
-                duration: 1.5,
-                ease: [1, 0.15, 0.6, 0.62],
-              }}
-              className="text-secondaryForeground flex bg-secondary items-center justify-center h-16 w-16 rounded-xl"
-            >
-              <ArrowBigRight size={8} className="h-8 w-8" />
-            </motion.div>
-          </div>
+          {children}
         </motion.button>
       </div>
-
-      <svg
-        className="absolute hidden"
-        width="0"
-        height="0"
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-      >
-        <defs>
-          <filter id="gooey">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation="10"
-              result="blur"
-            />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10"
-              result="gooey"
-            />
-            <feComposite in="SourceGraphic" in2="gooey" operator="atop" />
-          </filter>
-        </defs>
-      </svg>
     </>
   );
 };
